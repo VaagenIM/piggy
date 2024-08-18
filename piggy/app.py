828ce0a9ca4be95d73bfc65a5a22a71a576226e6
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-08-18 16:20:24 +0200

    Use lru_cache for assignments, abspath, oppgave -> assignment, uri -> path

--- piggy/app.py
+++ piggy/app.py
@@ -2,13 +2,13 @@ import os
 from functools import lru_cache
 from pathlib import Path
 
-from flask import Flask, send_file, request, render_template
+from flask import Flask, send_file, request, render_template, Response
 from turtleconverter import generate_static_files, mdfile_to_sections, ConversionError
 
 from piggy.piggybank import generate_piggymap
 
 # Ensure the working directory is the root of the project
-os.chdir(os.path.dirname(Path(__file__).parent))
+os.chdir(os.path.dirname(Path(__file__).parent.absolute()))
 
 PIGGYBANK_FOLDER = Path('piggybank')
 PIGGYMAP = generate_piggymap(PIGGYBANK_FOLDER)
@@ -27,13 +27,13 @@ MEDIA_URL_PREFIX = 'img'
 
 
 # TODO: Logging
+# TODO: Split up certain functions in this file into separate files
 
-
-def get_piggymap_segment_from_uri(uri: str) -> tuple[dict, dict]:
-    """Get the metadata and segment from a URI."""
+def get_piggymap_segment_from_path(path: str) -> tuple[dict, dict]:
+    """Get the metadata and segment from a path."""
     segment = dict(PIGGYMAP.copy())
     meta = segment.get('meta', {})
-    for path in uri.split('/'):
+    for path in path.split('/'):
         if not path:
             continue
         if path not in segment:
@@ -44,8 +44,8 @@ def get_piggymap_segment_from_uri(uri: str) -> tuple[dict, dict]:
     return meta, segment
 
 
-def get_directory_name_from_uri(uri: str) -> str:
-    """Get the directory name from a URI."""
+def get_directory_name_from_path(path: str) -> str:
+    """Get the directory name from a path."""
     # TODO: Use an enum?
     nest_level_dirname = {
         0: 'assignments_root',
@@ -54,8 +54,27 @@ def get_directory_name_from_uri(uri: str) -> str:
         3: 'subject',
         4: 'topic',
     }
-    uri = uri.split('/')
-    return nest_level_dirname.get(len([x for x in uri if x]), 'unknown')
+    path = path.split('/')
+    return nest_level_dirname.get(len([x for x in path if x]), 'unknown')
+
+
+@lru_cache
+def _render_assignment(p: Path) -> Response:
+    """Render an assignment from a Path object."""
+    if not p.exists():
+        # TODO: Raise a custom error
+        return Response('Error: Assignment not found', status=404)
+    try:
+        sections = mdfile_to_sections(p)
+        print('Rendering:', p)
+    except ConversionError:
+        # TODO: Raise a custom error
+        return Response('Error: Could not render assignment', status=500)
+    render = render_template('assignment.html',
+                             content=sections,
+                             current_language=SUPPORTED_LANGUAGES.get('name', 'Unknown'),
+                             supported_languages=SUPPORTED_LANGUAGES)
+    return Response(render, mimetype='text/html', status=200)
 
 
 def create_app():
@@ -73,65 +92,44 @@ def create_app():
         html += f'\n<a href="/{ASSIGNMENT_URL_PREFIX}"><button>Oppgaver</button></a>'
         return html
 
-    @app.route(f'/{ASSIGNMENT_URL_PREFIX}/<path:uri>')
+    @app.route(f'/{ASSIGNMENT_URL_PREFIX}/<path:path>')
     @app.route(f'/{ASSIGNMENT_URL_PREFIX}/')
     # @lru_cache
-    def render_assignment_directories(uri=''):
+    def render_assignment_directories(path=''):
         """
-        Render the webpage for a given URI.
+        Render the webpage for a given path.
 
-        Keeps track of template_type (assignments_root, year_level, class_name, subject, topic) based on the URI.
-        Gets the relevant piggymap from the URI. (key = child content, value = dict with relevant child data + meta)
+        Keeps track of template_type (assignments_root, year_level, class_name, subject, topic) based on the path.
+        Gets the relevant piggymap from the path. (key = child content, value = dict with relevant child data + meta)
         """
-        uri = uri.strip('/')
+        path = path.strip('/')
 
-        template_type = get_directory_name_from_uri(uri)
-        metadata, segment = get_piggymap_segment_from_uri(uri)
+        template_type = get_directory_name_from_path(path)
+        metadata, segment = get_piggymap_segment_from_path(path)
 
         return render_template(f'{template_type}.html',
                                meta=metadata,
                                segment=segment,
-                               uri=uri,
+                               path=path,
                                piggymap=PIGGYMAP)
 
-    assignment_cache = {}
-
-    @app.route(f'/{ASSIGNMENT_URL_PREFIX}/<year>/<class_name>/<subject>/<topic>/<assignment>')
-    # Uses its own cache
-    def render_assignment(year, class_name, subject, topic, assignment, lang=''):
+    @app.route(f'/{ASSIGNMENT_URL_PREFIX}/<year_level>/<class_name>/<subject>/<topic>/<assignment>')
+    def render_assignment(year_level, class_name, subject, topic, assignment, lang=''):
         """
         Render an assignment from the piggymap. Takes precedence over the wildcard route due to specificity.
         """
-        if request:
-            lang = request.cookies.get('lang')
+        # TODO: Simplify path if possible
+        if request:  # Caching doesn't work with request context
+            lang = request.cookies.get('lang', lang)
         if lang:
             assignment = f'translations/{lang}/{assignment}'
+        path = f'{PIGGYBANK_FOLDER}/{year_level}/{class_name}/{subject}/{topic}'
 
-        path = Path(f'{PIGGYBANK_FOLDER}/{year}/{class_name}/{subject}/{topic}/{assignment}.md')
-
-        # If the assignment has already been rendered, we can just return that
-        if path in assignment_cache:
-            return assignment_cache[path]
-
-        try:
-            sections = mdfile_to_sections(path)
-        except ConversionError:
-            # TODO: If the assignment exists but the translation is missing, we should show a message to the user
-            #       and allow them to change the language.
-            output = 'Translation not found', 404
-            assignment_cache[path] = output
-            return output
-
-        final_render = render_template('oppgave.html',
-                                       content=sections,
-                                       current_language=SUPPORTED_LANGUAGES.get(lang, {}).get('name', 'Unknown'),
-                                       supported_languages=SUPPORTED_LANGUAGES)
-        assignment_cache[path] = final_render
-        print('Assignment rendered:', path)
-        return final_render
+        return _render_assignment(Path(f'{path}/{assignment}.md'))
 
     def cache_all_assignments():
         """Reduces load time by caching all assignments on startup."""
+        # TODO: This is hacky. Find a better way to do this.
         with app.app_context():
             for year, year_data in PIGGYMAP.items():
                 render_assignment_directories(year)
@@ -159,7 +157,7 @@ def create_app():
         else:
             folder = 'attachments'
         try:
-            return send_file(Path(f'{PIGGYBANK_FOLDER}/{wildcard}/{folder}/{filename}'))
+            return send_file(Path(f'{PIGGYBANK_FOLDER}/{wildcard}/{folder}/{filename}').absolute())
         except FileNotFoundError:
             return send_file('static/img/placeholders/100x100.png')
 
