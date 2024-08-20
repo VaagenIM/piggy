751a980bd9443fdf4441978fa3f923c1e891acb4
svHvidsten <sindrehvidsten@gmail.com>
2024-08-20 13:50:32 +0200

    prevent run.py from swapping piggybank submodule branch in feature/front-end branch

--- run.py
+++ run.py
@@ -3,11 +3,11 @@ import os
 from piggy.app import create_app
 
 if __name__ == '__main__':
-    os.system('cd piggybank && git checkout test-data && cd .. && git submodule update --remote --recursive')
+    # os.system('cd piggybank && git checkout test-data && cd .. && git submodule update --remote --recursive')
     os.environ['FLASK_DEBUG'] = '1'
     os.environ['USE_CACHE'] = '0'
     app = create_app()
     app.run(port=5001)
 else:
-    os.system('cd piggybank && git checkout output && cd .. && git submodule update --remote --recursive')
+    # os.system('cd piggybank && git checkout output && cd .. && git submodule update --remote --recursive')
     app = create_app()
