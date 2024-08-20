8348321665db7b195d970987b81d9a8d04d42797
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-08-20 15:06:20 +0200

    Add tailwindcss

--- run.py
+++ run.py
@@ -1,13 +1,27 @@
 import os
+import subprocess
 
 from piggy.app import create_app
 
+TAILWIND_RUN_CMD = ('cd piggy && '
+                    'npx tailwindcss -c tailwind.config.js '
+                    '-i static/css/tailwind.input.css '
+                    '-o static/css/tailwind.css && '
+                    'cd ..')
+
+subprocess.Popen(TAILWIND_RUN_CMD, shell=True)
+
+
+def checkout_branch(branch):
+    os.system(f'cd piggybank && git checkout {branch} && cd .. && git submodule update --remote --recursive')
+
+
 if __name__ == '__main__':
-    # os.system('cd piggybank && git checkout test-data && cd .. && git submodule update --remote --recursive')
+    # checkout_branch('test-data')
     os.environ['FLASK_DEBUG'] = '1'
     os.environ['USE_CACHE'] = '0'
     app = create_app()
     app.run(port=5001)
 else:
-    # os.system('cd piggybank && git checkout output && cd .. && git submodule update --remote --recursive')
+    checkout_branch('output')
     app = create_app()
