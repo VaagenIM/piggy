19d2a3c67848bb8bef9ce038463ba5d791d2bb35
svHvidsten <sindrehvidsten@gmail.com>
2024-08-23 11:46:28 +0200

    Disabled live reload, it was buggy

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ from piggy.app import create_app
 def run_tailwind(reload=False):
     cmd = (
         "cd piggy && "
-        f"npx tailwindcss {'--watch ' if reload else ''}"
+        f"npx tailwindcss {'--watch' if reload else ''}"
         "-c tailwind.config.js "
         "-i static/css/tailwind.input.css "
         "-o static/css/tailwind.css && "
@@ -30,14 +30,22 @@ if __name__ == "__main__":
     os.environ["USE_CACHE"] = "0"
 
     app = create_app()
-    inject_devtools(app)
+
+    if not os.environ.get("injected", False):
+        inject_devtools(app)
+
+    os.environ["injected"] = "1"
 
     run_tailwind(reload=True)
 
+    """
     server = Server(app)
     server.watch("piggy/**/*", ignore=lambda *_: False)
     server.watch("piggybank/*", ignore=lambda *_: False)
     server.serve(host="127.0.0.1", port=5001, debug=False)
+    """
+
+    app.run(port=5001)
 else:
     checkout_branch("output")
     run_tailwind()
