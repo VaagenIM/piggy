8c1138046e192d506252938f1f0709a7d481527e
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-09-03 17:10:07 +0200

    ci: reuse attachments in translated assignments in pages

--- .github/workflows/web_scraper.py
+++ .github/workflows/web_scraper.py
@@ -33,15 +33,17 @@ def get_html(link):
         html = r.text
 
     new_links = get_links(html, path=link.strip("/"))
-    new_media_links = get_media_links(html, path=link.strip("/"))
 
     for l in new_links:
         if l not in links:
             links.append(l)
 
-    for l in new_media_links:
-        if l not in media_links:
-            media_links.append(l)
+    # Get media links as long as we are not in the lang folder
+    if "/lang/" not in link:
+        new_media_links = get_media_links(html, path=link.strip("/"))
+        for l in new_media_links:
+            if l not in media_links:
+                media_links.append(l)
 
     visited.append(link)
 
@@ -50,6 +52,11 @@ def get_html(link):
 
     # Replace all content (og) links with the cname
     html = re.sub(rf"content=\"({url})([^\"/]*)", rf'content="{cname}\2', html)
+
+    # A hack to fix media links
+    if "/lang/" in link:
+        html = re.sub(r'href="attachments/', 'href="../../attachments/', html)
+        html = re.sub(r'src="attachments/', 'src="../../attachments/', html)
 
     return html
 
