dcdc984f3ad99f2026ef881715ab15557ee157c6
svHvidsten <sindrehvidsten@gmail.com>
2024-09-12 17:52:14 +0200

    extracted themes from 'styles.css' and created separate files for each theme, also added tooltips to theme buttons and cleaned up some theme colors

--- piggy/utils.py
+++ piggy/utils.py
@@ -64,13 +64,59 @@ def normalize_url_str(text: str) -> str:
     return re.sub("_+", "_", new_text)
 
 
+THEME_PATH = "piggy/static/css/themes"
+
 def get_themes():
-    return os.listdir("piggy/static/css/themes")
+    themes = os.listdir(THEME_PATH)
+
+    theme_output = []
+
+    for theme in themes:
+        theme_data = get_css_metadata(THEME_PATH + f"/{theme}")
+
+        if theme_data is None:
+            continue
+
+        theme_output.append(theme_data)
+    
+    return sorted(theme_output, key=lambda d: d['id'])
 
 
-def get_theme_metadata(path: str):
-    theme_data = {}
+# quick and dirty state_machine to read CSS metadata
+class ParserState():
+    INIT = 1
+    READ = 2
 
-    return
-    # with theme_path.open() as file:
-    #     return None
+CSS_META_IDENTIFIER = "/* METADATA"
+
+def get_css_metadata(path: str):
+    css_metadata = {}
+    css_path = Path(path)
+
+    if css_path.suffix != '.css':
+        return None
+
+    valid = True
+    state = ParserState.INIT
+
+    with css_path.open() as file:
+        while valid:
+            line = file.readline()
+
+            match state:
+                case ParserState.INIT:
+                    if str(line).startswith(CSS_META_IDENTIFIER):
+                        state = ParserState.READ
+                    else:
+                        valid = False
+                case ParserState.READ:
+                    meta_item = line.split(':', 1)
+
+                    if len(meta_item) < 2:
+                        valid = False
+                    else:
+                        css_metadata[meta_item[0].strip()] = meta_item[1].strip()
+
+    css_metadata["path"] = css_path.stem
+
+    return css_metadata
