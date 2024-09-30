898384ef05b48837fae2efd85dc757d13dd7301c
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2024-09-30 23:12:32 +0200

    chore: run prettier formatting

--- piggy/static/js/card_script.js
+++ piggy/static/js/card_script.js
@@ -1,45 +1,46 @@
 function checkTagsVisibility() {
-    const tagsContainers = document.querySelectorAll('.tags-container');
-    tagsContainers.forEach(container => {
-        const seeMoreButton = container.nextElementSibling;
-        if (container.scrollHeight > container.clientHeight) {
-            seeMoreButton.style.display = 'block';
-            container.classList.add('fade');
-        } else {
-            seeMoreButton.style.display = 'none';
-            container.classList.remove('fade');
-        }
-    });
+  const tagsContainers = document.querySelectorAll(".tags-container");
+  tagsContainers.forEach((container) => {
+    const seeMoreButton = container.nextElementSibling;
+    if (container.scrollHeight > container.clientHeight) {
+      seeMoreButton.style.display = "block";
+      container.classList.add("fade");
+    } else {
+      seeMoreButton.style.display = "none";
+      container.classList.remove("fade");
+    }
+  });
 }
 
 function toggleTags(button) {
-    const tagsContainer = button.previousElementSibling;
-    tagsContainer.classList.toggle('expanded');
-    button.textContent = tagsContainer.classList.contains('expanded') ? 'Se færre' : 'Se flere';
+  const tagsContainer = button.previousElementSibling;
+  tagsContainer.classList.toggle("expanded");
+  button.textContent = tagsContainer.classList.contains("expanded")
+    ? "Se færre"
+    : "Se flere";
 }
 
 // Function to generate a color from a string (tag text)
-function stringToColor(str, transparency = 'FF') {
-    let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-    }
-    let color = '#';
-    for (let i = 0; i < 3; i++) {
-        let value = (hash >> (i * 8)) & 0xFF;
-        color += ('00' + value.toString(16)).slice(-2);
-    }
+function stringToColor(str, transparency = "FF") {
+  let hash = 0;
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  let color = "#";
+  for (let i = 0; i < 3; i++) {
+    let value = (hash >> (i * 8)) & 0xff;
+    color += ("00" + value.toString(16)).slice(-2);
+  }
 
-    color += transparency;
+  color += transparency;
 
-    return color;
+  return color;
 }
 
-const tags = document.querySelectorAll('.tag-box');
+const tags = document.querySelectorAll(".tag-box");
 
-tags.forEach(tag => {
-    const tagText = tag.getAttribute('data-tag');
-    tag.style.backgroundColor = stringToColor(tagText, '88');
-    tag.style.borderColor = stringToColor(tagText);
+tags.forEach((tag) => {
+  const tagText = tag.getAttribute("data-tag");
+  tag.style.backgroundColor = stringToColor(tagText, "88");
+  tag.style.borderColor = stringToColor(tagText);
 });
-
--- piggy/static/js/main.js
+++ piggy/static/js/main.js
@@ -1,85 +1,85 @@
 // Get elements
-const settingsMenu = document.getElementById('settings-menu');
-const settingsBtn = document.getElementById('settings-btn');
-const closeBtn = document.querySelector('.settings-menu .closebtn');
-const themeSelect = document.getElementById('theme-select');
-const dyslexiaBtn = document.getElementById('dyslexia-button');
+const settingsMenu = document.getElementById("settings-menu");
+const settingsBtn = document.getElementById("settings-btn");
+const closeBtn = document.querySelector(".settings-menu .closebtn");
+const themeSelect = document.getElementById("theme-select");
+const dyslexiaBtn = document.getElementById("dyslexia-button");
 
-const currentTheme = localStorage.getItem('theme') || 'dark';
-const fontTheme = localStorage.getItem('data-font-theme') || 'default';
+const currentTheme = localStorage.getItem("theme") || "dark";
+const fontTheme = localStorage.getItem("data-font-theme") || "default";
 
 function pageTransition() {
-    document.body.classList.add("transition");
-    setTimeout(() => {
-        document.body.classList.remove("transition");
-    }, 1000);
+  document.body.classList.add("transition");
+  setTimeout(() => {
+    document.body.classList.remove("transition");
+  }, 1000);
 }
 
 // Function to open the settings menu
 function openSettingsMenu() {
-    settingsMenu.classList.add('open');
+  settingsMenu.classList.add("open");
 }
 
 // Function to close the settings menu
 function closeSettingsMenu() {
-    settingsMenu.classList.remove('open');
+  settingsMenu.classList.remove("open");
 }
 
 function toggleDyslexia() {
-    // Retrieve the current theme from the data attribute
-    let fontTheme = document.documentElement.getAttribute('data-font-theme');
+  // Retrieve the current theme from the data attribute
+  let fontTheme = document.documentElement.getAttribute("data-font-theme");
 
-    // Toggle between 'default' and 'dyslexia'
-    if (fontTheme === "default") {
-        document.documentElement.setAttribute('data-font-theme', 'dyslexia');
-        localStorage.setItem('data-font-theme', 'dyslexia'); // Save theme to localStorage
-        dyslexiaBtn.innerHTML = "Dyslexia Friendly Mode [✅]"
-    } else {
-        document.documentElement.setAttribute('data-font-theme', 'default');
-        localStorage.setItem('data-font-theme', 'default'); // Save theme to localStorage
-        dyslexiaBtn.innerHTML = "Dyslexia Friendly Mode"
-    }
+  // Toggle between 'default' and 'dyslexia'
+  if (fontTheme === "default") {
+    document.documentElement.setAttribute("data-font-theme", "dyslexia");
+    localStorage.setItem("data-font-theme", "dyslexia"); // Save theme to localStorage
+    dyslexiaBtn.innerHTML = "Dyslexia Friendly Mode [✅]";
+  } else {
+    document.documentElement.setAttribute("data-font-theme", "default");
+    localStorage.setItem("data-font-theme", "default"); // Save theme to localStorage
+    dyslexiaBtn.innerHTML = "Dyslexia Friendly Mode";
+  }
 
-    pageTransition();
+  pageTransition();
 }
 
 // Set the current theme on page load
-document.documentElement.setAttribute('data-theme', currentTheme);
-document.documentElement.setAttribute('data-font-theme', fontTheme);
+document.documentElement.setAttribute("data-theme", currentTheme);
+document.documentElement.setAttribute("data-font-theme", fontTheme);
 
 if (fontTheme === "default") {
-    dyslexiaBtn.innerHTML = "Dyslexia Friendly Mode"
+  dyslexiaBtn.innerHTML = "Dyslexia Friendly Mode";
 } else {
-    dyslexiaBtn.innerHTML = "Dyslexia Friendly Mode [✔]"
+  dyslexiaBtn.innerHTML = "Dyslexia Friendly Mode [✔]";
 }
 
 // Set the selected option based on the current theme
 themeSelect.value = currentTheme;
 
 // Event listener for the Settings button
-settingsBtn.addEventListener('click', openSettingsMenu);
+settingsBtn.addEventListener("click", openSettingsMenu);
 
 // Event listener for the Close button inside the menu
-closeBtn.addEventListener('click', closeSettingsMenu);
+closeBtn.addEventListener("click", closeSettingsMenu);
 
-dyslexiaBtn.addEventListener('click', toggleDyslexia);
+dyslexiaBtn.addEventListener("click", toggleDyslexia);
 
 // Event listener for theme selection change
-themeSelect.addEventListener('change', function() {
-    pageTransition();
+themeSelect.addEventListener("change", function () {
+  pageTransition();
 
-    const selectedTheme = themeSelect.value;
-    document.documentElement.setAttribute('data-theme', selectedTheme);
-    localStorage.setItem('theme', selectedTheme); // Save theme to localStorage
+  const selectedTheme = themeSelect.value;
+  document.documentElement.setAttribute("data-theme", selectedTheme);
+  localStorage.setItem("theme", selectedTheme); // Save theme to localStorage
 });
 
 // Close settings menu when clicking outside of it
-window.addEventListener('click', function(event) {
-    if (
-        settingsMenu.classList.contains('open') && // Only if the menu is open
-        !settingsMenu.contains(event.target) && // Click is outside the menu
-        event.target !== settingsBtn // Click is not on the settings button
-    ) {
-        closeSettingsMenu();
-    }
+window.addEventListener("click", function (event) {
+  if (
+    settingsMenu.classList.contains("open") && // Only if the menu is open
+    !settingsMenu.contains(event.target) && // Click is outside the menu
+    event.target !== settingsBtn // Click is not on the settings button
+  ) {
+    closeSettingsMenu();
+  }
 });
