ae0b93c67ba55d9791f95b2142e1d500399fdad2
svHvidsten <sindrehvidsten@gmail.com>
2024-10-04 14:15:07 +0200

    Added a "flag" field to the languages.

--- piggy/models.py
+++ piggy/models.py
@@ -1,208 +1,208 @@
 LANGUAGES = {
-    "": {"name": "Norsk"},  # Default language
+    "": {"name": "Norsk", "flag": "🇳🇴"},  # Default language
 
-    "aar": {"name": "Afaraf"},
-    "abk": {"name": "Аҧсуа"},
-    "ace": {"name": "Bahsa Acèh"},
-    "ach": {"name": "Lwo"},
-    "ada": {"name": "Adangme"},
-    "ady": {"name": "Адыгэбзэ"},
-    "afr": {"name": "Afrikaans"},
-    "ain": {"name": "アイヌ・イタㇰ"},
-    "aka": {"name": "Akan"},
-    "alb": {"name": "Shqip"},  # Also 'sqi'
-    "amh": {"name": "አማርኛ"},
-    "ara": {"name": "العربية"},
-    "arg": {"name": "Aragonés"},
-    "arm": {"name": "Հայերեն"},  # Also 'hye'
-    "asm": {"name": "অসমীয়া"},
-    "ava": {"name": "Авар мацӀ"},
-    "ave": {"name": "avesta"},
-    "aym": {"name": "Aymar aru"},
-    "aze": {"name": "Azərbaycanca"},
-    "bak": {"name": "Башҡорт теле"},
-    "bam": {"name": "Bamanankan"},
-    "baq": {"name": "Euskara"},  # Also 'eus'
-    "bel": {"name": "Беларуская"},
-    "ben": {"name": "বাংলা"},
-    "bih": {"name": "भोजपुरी"},
-    "bis": {"name": "Bislama"},
-    "bod": {"name": "བོད་ཡིག"},  # Tibetan
-    "bos": {"name": "Bosanski"},
-    "bre": {"name": "Brezhoneg"},
-    "bul": {"name": "Български"},
-    "bur": {"name": "မြန်မာစာ"},  # Also 'mya'
-    "cat": {"name": "Català"},
-    "ces": {"name": "Čeština"},
-    "cha": {"name": "Chamoru"},
-    "che": {"name": "нохчийн мотт"},
-    "chi": {"name": "中文"},  # Also 'zho'
-    "chu": {"name": "Словѣньскъ"},
-    "chv": {"name": "Чӑвашла"},
-    "cor": {"name": "Kernewek"},
-    "cos": {"name": "Corsu"},
-    "cre": {"name": "Nehiyaw"},
-    "cym": {"name": "Cymraeg"},
-    "dan": {"name": "Dansk"},
-    "deu": {"name": "Deutsch"},
-    "div": {"name": "ދިވެހި"},
-    "dut": {"name": "Nederlands"},  # Also 'nld'
-    "dzo": {"name": "རྫོང་ཁ"},
-    "ell": {"name": "Ελληνικά"},  # Also 'gre'
-    "eng": {"name": "English"},
-    "epo": {"name": "Esperanto"},
-    "est": {"name": "Eesti"},
-    "eus": {"name": "Euskara"},
-    "ewe": {"name": "Eʋegbe"},
-    "fao": {"name": "Føroyskt"},
-    "fas": {"name": "فارسی"},  # Also 'per'
-    "fij": {"name": "Na Vosa Vakaviti"},
-    "fin": {"name": "Suomi"},
-    "fra": {"name": "Français"},
-    "fre": {"name": "Français"},  # Also 'fra'
-    "fry": {"name": "Frysk"},
-    "ful": {"name": "Fulfulde"},
-    "geo": {"name": "ქართული"},  # Also 'kat'
-    "ger": {"name": "Deutsch"},  # Also 'deu'
-    "gla": {"name": "Gàidhlig"},
-    "gle": {"name": "Gaeilge"},
-    "glg": {"name": "Galego"},
-    "glv": {"name": "Gaelg"},
-    "gre": {"name": "Ελληνικά"},
-    "grn": {"name": "Avañe'ẽ"},
-    "guj": {"name": "ગુજરાતી"},
-    "hat": {"name": "Kreyòl ayisyen"},
-    "hau": {"name": "Hausa"},
-    "heb": {"name": "עברית"},
-    "her": {"name": "Otjiherero"},
-    "hin": {"name": "हिन्दी"},
-    "hmo": {"name": "Hiri Motu"},
-    "hrv": {"name": "Hrvatski"},
-    "hun": {"name": "Magyar"},
-    "hye": {"name": "Հայերեն"},
-    "ibo": {"name": "Igbo"},
-    "ice": {"name": "Íslenska"},  # Also 'isl'
-    "ido": {"name": "Ido"},
-    "iii": {"name": "ꆈꌠ꒿ Nuosuhxop"},
-    "iku": {"name": "ᐃᓄᒃᑎᑐᑦ"},
-    "ind": {"name": "Bahasa Indonesia"},
-    "ipk": {"name": "Iñupiatun"},
-    "isl": {"name": "Íslenska"},
-    "ita": {"name": "Italiano"},
-    "jav": {"name": "Basa Jawa"},
-    "jpn": {"name": "日本語"},
-    "kal": {"name": "Kalaallisut"},
-    "kan": {"name": "ಕನ್ನಡ"},
-    "kas": {"name": "कश्मीरी"},
-    "kat": {"name": "ქართული"},
-    "kau": {"name": "Kanuri"},
-    "kaz": {"name": "Қазақша"},
-    "khm": {"name": "ភាសាខ្មែរ"},
-    "kik": {"name": "Gĩkũyũ"},
-    "kin": {"name": "Ikinyarwanda"},
-    "kir": {"name": "Кыргызча"},
-    "kom": {"name": "Коми кыв"},
-    "kon": {"name": "Kikongo"},
-    "kor": {"name": "한국어"},
-    "kua": {"name": "Kuanyama"},
-    "kur": {"name": "Kurdî"},
-    "lao": {"name": "ພາສາລາວ"},
-    "lat": {"name": "Latina"},
-    "lav": {"name": "Latviešu"},
-    "lim": {"name": "Limburgs"},
-    "lin": {"name": "Lingála"},
-    "lit": {"name": "Lietuvių"},
-    "ltz": {"name": "Lëtzebuergesch"},
-    "lub": {"name": "Kiluba"},
-    "lug": {"name": "Luganda"},
-    "mac": {"name": "Македонски"},  # Also 'mkd'
-    "mah": {"name": "Kajin M̧ajeļ"},
-    "mal": {"name": "മലയാളം"},
-    "mar": {"name": "मराठी"},
-    "mkd": {"name": "Македонски"},
-    "mlg": {"name": "Malagasy"},
-    "mlt": {"name": "Malti"},
-    "mon": {"name": "Монгол"},
-    "mri": {"name": "Te Reo Māori"},  # Also 'mao'
-    "msa": {"name": "Bahasa Melayu"},
-    "mya": {"name": "မြန်မာစာ"},
-    "nau": {"name": "Ekakairũ Naoero"},
-    "nav": {"name": "Diné bizaad"},
-    "nbl": {"name": "isiNdebele"},
-    "nde": {"name": "isiNdebele"},
-    "ndo": {"name": "Owambo"},
-    "nep": {"name": "नेपाली"},
-    "nld": {"name": "Nederlands"},
-    "nno": {"name": "Norsk nynorsk"},
-    "nob": {"name": "Norsk bokmål"},
-    "nor": {"name": "Norsk"},
-    "nya": {"name": "ChiCheŵa"},
-    "oci": {"name": "Occitan"},
-    "oji": {"name": "ᐊᓂᔑᓈᐯᒧᐎᓐ"},
-    "ori": {"name": "ଓଡ଼ିଆ"},
-    "orm": {"name": "Afaan Oromoo"},
-    "oss": {"name": "Ирон æвзаг"},
-    "pan": {"name": "ਪੰਜਾਬੀ"},
-    "per": {"name": "فارسی"},
-    "pli": {"name": "पाऴि"},
-    "pol": {"name": "Polski"},
-    "por": {"name": "Português"},
-    "pus": {"name": "پښتو"},
-    "que": {"name": "Runa Simi"},
-    "roh": {"name": "Rumantsch"},
-    "ron": {"name": "Română"},
-    "rum": {"name": "Română"},  # Also 'ron'
-    "run": {"name": "Kirundi"},
-    "rus": {"name": "Русский"},
-    "sag": {"name": "Sängö"},
-    "san": {"name": "संस्कृतम्"},
-    "sin": {"name": "සිංහල"},
-    "slk": {"name": "Slovenčina"},
-    "slo": {"name": "Slovenčina"},  # Also 'slk'
-    "slv": {"name": "Slovenščina"},
-    "sme": {"name": "Davvisámegiella"},
-    "smo": {"name": "Gagana Samoa"},
-    "sna": {"name": "ChiShona"},
-    "snd": {"name": "سنڌي"},
-    "som": {"name": "Soomaaliga"},
-    "sot": {"name": "Sesotho"},
-    "spa": {"name": "Español"},
-    "sqi": {"name": "Shqip"},
-    "srd": {"name": "Sardu"},
-    "srp": {"name": "Српски"},
-    "ssw": {"name": "SiSwati"},
-    "sun": {"name": "Basa Sunda"},
-    "swa": {"name": "Kiswahili"},
-    "swe": {"name": "Svenska"},
-    "tah": {"name": "Reo Tahiti"},
-    "tam": {"name": "தமிழ்"},
-    "tat": {"name": "Татар теле"},
-    "tel": {"name": "తెలుగు"},
-    "tgk": {"name": "Тоҷикӣ"},
-    "tgl": {"name": "Tagalog"},
-    "tha": {"name": "ไทย"},
-    "tib": {"name": "བོད་ཡིག"},
-    "tir": {"name": "ትግርኛ"},
-    "ton": {"name": "faka Tonga"},
-    "tsn": {"name": "Setswana"},
-    "tso": {"name": "Xitsonga"},
-    "tuk": {"name": "Türkmençe"},
-    "tur": {"name": "Türkçe"},
-    "twi": {"name": "Twi"},
-    "uig": {"name": "Uyƣurqə"},
-    "ukr": {"name": "Українська"},
-    "urd": {"name": "اردو"},
-    "uzb": {"name": "Oʻzbek"},
-    "ven": {"name": "Tshivenḓa"},
-    "vie": {"name": "Tiếng Việt"},
-    "vol": {"name": "Volapük"},
-    "wel": {"name": "Cymraeg"},  # Also 'cym'
-    "wln": {"name": "Walon"},
-    "wol": {"name": "Wolof"},
-    "xho": {"name": "isiXhosa"},
-    "yid": {"name": "ייִדיש"},
-    "yor": {"name": "Yorùbá"},
-    "zha": {"name": "Saɯ cueŋƅ"},
-    "zho": {"name": "中文"},
-    "zul": {"name": "isiZulu"},
+    "aar": {"name": "Afaraf", "flag": "🇪🇷"},  # Eritrea
+    "abk": {"name": "Аҧсуа", "flag": "🇬🇪"},  # Georgia (Abkhazia)
+    "ace": {"name": "Bahsa Acèh", "flag": "🇮🇩"},  # Indonesia
+    "ach": {"name": "Lwo", "flag": "🇺🇬"},  # Uganda
+    "ada": {"name": "Adangme", "flag": "🇬🇭"},  # Ghana
+    "ady": {"name": "Адыгэбзэ", "flag": "🇷🇺"},  # Russia
+    "afr": {"name": "Afrikaans", "flag": "🇿🇦"},  # South Africa
+    "ain": {"name": "アイヌ・イタㇰ", "flag": "🇯🇵"},  # Japan
+    "aka": {"name": "Akan", "flag": "🇬🇭"},  # Ghana
+    "alb": {"name": "Shqip", "flag": "🇦🇱"},  # Albania
+    "amh": {"name": "አማርኛ", "flag": "🇪🇹"},  # Ethiopia
+    "ara": {"name": "العربية", "flag": "🇸🇦"},  # Saudi Arabia
+    "arg": {"name": "Aragonés", "flag": "🇪🇸"},  # Spain
+    "arm": {"name": "Հայերեն", "flag": "🇦🇲"},  # Armenia
+    "asm": {"name": "অসমীয়া", "flag": "🇮🇳"},  # India
+    "ava": {"name": "Авар мацӀ", "flag": "🇷🇺"},  # Russia
+    "ave": {"name": "avesta", "flag": "🏳️"},  # Historical language
+    "aym": {"name": "Aymar aru", "flag": "🇧🇴"},  # Bolivia
+    "aze": {"name": "Azərbaycanca", "flag": "🇦🇿"},  # Azerbaijan
+    "bak": {"name": "Башҡорт теле", "flag": "🇷🇺"},  # Russia
+    "bam": {"name": "Bamanankan", "flag": "🇲🇱"},  # Mali
+    "baq": {"name": "Euskara", "flag": "🇪🇸"},  # Spain
+    "bel": {"name": "Беларуская", "flag": "🇧🇾"},  # Belarus
+    "ben": {"name": "বাংলা", "flag": "🇧🇩"},  # Bangladesh
+    "bih": {"name": "भोजपुरी", "flag": "🇮🇳"},  # India
+    "bis": {"name": "Bislama", "flag": "🇻🇺"},  # Vanuatu
+    "bod": {"name": "བོད་ཡིག", "flag": "🇨🇳"},  # China (Tibet)
+    "bos": {"name": "Bosanski", "flag": "🇧🇦"},  # Bosnia and Herzegovina
+    "bre": {"name": "Brezhoneg", "flag": "🇫🇷"},  # France (Brittany)
+    "bul": {"name": "Български", "flag": "🇧🇬"},  # Bulgaria
+    "bur": {"name": "မြန်မာစာ", "flag": "🇲🇲"},  # Myanmar
+    "cat": {"name": "Català", "flag": "🇪🇸"},  # Spain
+    "ces": {"name": "Čeština", "flag": "🇨🇿"},  # Czech Republic
+    "cha": {"name": "Chamoru", "flag": "🇬🇺"},  # Guam
+    "che": {"name": "нохчийн мотт", "flag": "🇷🇺"},  # Russia (Chechnya)
+    "chi": {"name": "中文", "flag": "🇨🇳"},  # China
+    "chu": {"name": "Словѣньскъ", "flag": "🏳️"},  # Historical language
+    "chv": {"name": "Чӑвашла", "flag": "🇷🇺"},  # Russia
+    "cor": {"name": "Kernewek", "flag": "🏴󠁧󠁢󠁥󠁮󠁧󠁿"},  # England (Cornwall)
+    "cos": {"name": "Corsu", "flag": "🇫🇷"},  # France (Corsica)
+    "cre": {"name": "Nehiyaw", "flag": "🇨🇦"},  # Canada
+    "cym": {"name": "Cymraeg", "flag": "🏴󠁧󠁢󠁷󠁬󠁳󠁿"},  # Wales
+    "dan": {"name": "Dansk", "flag": "🇩🇰"},  # Denmark
+    "deu": {"name": "Deutsch", "flag": "🇩🇪"},  # Germany
+    "div": {"name": "ދިވެހި", "flag": "🇲🇻"},  # Maldives
+    "dut": {"name": "Nederlands", "flag": "🇳🇱"},  # Netherlands
+    "dzo": {"name": "རྫོང་ཁ", "flag": "🇧🇹"},  # Bhutan
+    "ell": {"name": "Ελληνικά", "flag": "🇬🇷"},  # Greece
+    "eng": {"name": "English", "flag": "🇬🇧"},  # United Kingdom
+    "epo": {"name": "Esperanto", "flag": "🏳️"},  # Constructed language
+    "est": {"name": "Eesti", "flag": "🇪🇪"},  # Estonia
+    "eus": {"name": "Euskara", "flag": "🇪🇸"},  # Spain
+    "ewe": {"name": "Eʋegbe", "flag": "🇹🇬"},  # Togo
+    "fao": {"name": "Føroyskt", "flag": "🇫🇴"},  # Faroe Islands
+    "fas": {"name": "فارسی", "flag": "🇮🇷"},  # Iran
+    "fij": {"name": "Na Vosa Vakaviti", "flag": "🇫🇯"},  # Fiji
+    "fin": {"name": "Suomi", "flag": "🇫🇮"},  # Finland
+    "fra": {"name": "Français", "flag": "🇫🇷"},  # France
+    "fre": {"name": "Français", "flag": "🇫🇷"},  # Also 'fra'
+    "fry": {"name": "Frysk", "flag": "🇳🇱"},  # Netherlands
+    "ful": {"name": "Fulfulde", "flag": "🇳🇬"},  # Nigeria
+    "geo": {"name": "ქართული", "flag": "🇬🇪"},  # Georgia
+    "ger": {"name": "Deutsch", "flag": "🇩🇪"},  # Also 'deu'
+    "gla": {"name": "Gàidhlig", "flag": "🏴󠁧󠁢󠁳󠁣󠁴󠁿"},  # Scotland
+    "gle": {"name": "Gaeilge", "flag": "🇮🇪"},  # Ireland
+    "glg": {"name": "Galego", "flag": "🇪🇸"},  # Spain
+    "glv": {"name": "Gaelg", "flag": "🇮🇲"},  # Isle of Man
+    "gre": {"name": "Ελληνικά", "flag": "🇬🇷"},  # Also 'ell'
+    "grn": {"name": "Avañe'ẽ", "flag": "🇵🇾"},  # Paraguay
+    "guj": {"name": "ગુજરાતી", "flag": "🇮🇳"},  # India
+    "hat": {"name": "Kreyòl ayisyen", "flag": "🇭🇹"},  # Haiti
+    "hau": {"name": "Hausa", "flag": "🇳🇬"},  # Nigeria
+    "heb": {"name": "עברית", "flag": "🇮🇱"},  # Israel
+    "her": {"name": "Otjiherero", "flag": "🇳🇦"},  # Namibia
+    "hin": {"name": "हिन्दी", "flag": "🇮🇳"},  # India
+    "hmo": {"name": "Hiri Motu", "flag": "🇵🇬"},  # Papua New Guinea
+    "hrv": {"name": "Hrvatski", "flag": "🇭🇷"},  # Croatia
+    "hun": {"name": "Magyar", "flag": "🇭🇺"},  # Hungary
+    "hye": {"name": "Հայերեն", "flag": "🇦🇲"},  # Armenia
+    "ibo": {"name": "Igbo", "flag": "🇳🇬"},  # Nigeria
+    "ice": {"name": "Íslenska", "flag": "🇮🇸"},  # Also 'isl'
+    "ido": {"name": "Ido", "flag": "🏳️"},  # Constructed language
+    "iii": {"name": "ꆈꌠ꒿ Nuosuhxop", "flag": "🇨🇳"},  # China
+    "iku": {"name": "ᐃᓄᒃᑎᑐᑦ", "flag": "🇨🇦"},  # Canada
+    "ind": {"name": "Bahasa Indonesia", "flag": "🇮🇩"},  # Indonesia
+    "ipk": {"name": "Iñupiatun", "flag": "🇺🇸"},  # United States (Alaska)
+    "isl": {"name": "Íslenska", "flag": "🇮🇸"},  # Iceland
+    "ita": {"name": "Italiano", "flag": "🇮🇹"},  # Italy
+    "jav": {"name": "Basa Jawa", "flag": "🇮🇩"},  # Indonesia
+    "jpn": {"name": "日本語", "flag": "🇯🇵"},  # Japan
+    "kal": {"name": "Kalaallisut", "flag": "🇬🇱"},  # Greenland
+    "kan": {"name": "ಕನ್ನಡ", "flag": "🇮🇳"},  # India
+    "kas": {"name": "कश्मीरी", "flag": "🇮🇳"},  # India
+    "kat": {"name": "ქართული", "flag": "🇬🇪"},  # Georgia
+    "kau": {"name": "Kanuri", "flag": "🇳🇬"},  # Nigeria
+    "kaz": {"name": "Қазақша", "flag": "🇰🇿"},  # Kazakhstan
+    "khm": {"name": "ភាសាខ្មែរ", "flag": "🇰🇭"},  # Cambodia
+    "kik": {"name": "Gĩkũyũ", "flag": "🇰🇪"},  # Kenya
+    "kin": {"name": "Ikinyarwanda", "flag": "🇷🇼"},  # Rwanda
+    "kir": {"name": "Кыргызча", "flag": "🇰🇬"},  # Kyrgyzstan
+    "kom": {"name": "Коми кыв", "flag": "🇷🇺"},  # Russia
+    "kon": {"name": "Kikongo", "flag": "🇨🇩"},  # DR Congo
+    "kor": {"name": "한국어", "flag": "🇰🇷"},  # South Korea
+    "kua": {"name": "Kuanyama", "flag": "🇳🇦"},  # Namibia
+    "kur": {"name": "Kurdî", "flag": "🇹🇷"},  # Turkey
+    "lao": {"name": "ພາສາລາວ", "flag": "🇱🇦"},  # Laos
+    "lat": {"name": "Latina", "flag": "🇻🇦"},  # Vatican City
+    "lav": {"name": "Latviešu", "flag": "🇱🇻"},  # Latvia
+    "lim": {"name": "Limburgs", "flag": "🇳🇱"},  # Netherlands
+    "lin": {"name": "Lingála", "flag": "🇨🇩"},  # DR Congo
+    "lit": {"name": "Lietuvių", "flag": "🇱🇹"},  # Lithuania
+    "ltz": {"name": "Lëtzebuergesch", "flag": "🇱🇺"},  # Luxembourg
+    "lub": {"name": "Kiluba", "flag": "🇨🇩"},  # DR Congo
+    "lug": {"name": "Luganda", "flag": "🇺🇬"},  # Uganda
+    "mac": {"name": "Македонски", "flag": "🇲🇰"},  # North Macedonia
+    "mah": {"name": "Kajin M̧ajeļ", "flag": "🇲🇭"},  # Marshall Islands
+    "mal": {"name": "മലയാളം", "flag": "🇮🇳"},  # India
+    "mar": {"name": "मराठी", "flag": "🇮🇳"},  # India
+    "mkd": {"name": "Македонски", "flag": "🇲🇰"},  # North Macedonia
+    "mlg": {"name": "Malagasy", "flag": "🇲🇬"},  # Madagascar
+    "mlt": {"name": "Malti", "flag": "🇲🇹"},  # Malta
+    "mon": {"name": "Монгол", "flag": "🇲🇳"},  # Mongolia
+    "mri": {"name": "Te Reo Māori", "flag": "🇳🇿"},  # New Zealand
+    "msa": {"name": "Bahasa Melayu", "flag": "🇲🇾"},  # Malaysia
+    "mya": {"name": "မြန်မာစာ", "flag": "🇲🇲"},  # Myanmar
+    "nau": {"name": "Ekakairũ Naoero", "flag": "🇳🇷"},  # Nauru
+    "nav": {"name": "Diné bizaad", "flag": "🇺🇸"},  # United States
+    "nbl": {"name": "isiNdebele", "flag": "🇿🇦"},  # South Africa
+    "nde": {"name": "isiNdebele", "flag": "🇿🇼"},  # Zimbabwe
+    "ndo": {"name": "Owambo", "flag": "🇳🇦"},  # Namibia
+    "nep": {"name": "नेपाली", "flag": "🇳🇵"},  # Nepal
+    "nld": {"name": "Nederlands", "flag": "🇳🇱"},  # Netherlands
+    "nno": {"name": "Norsk nynorsk", "flag": "🇳🇴"},  # Norway
+    "nob": {"name": "Norsk bokmål", "flag": "🇳🇴"},  # Norway
+    "nor": {"name": "Norsk", "flag": "🇳🇴"},  # Norway
+    "nya": {"name": "ChiCheŵa", "flag": "🇲🇼"},  # Malawi
+    "oci": {"name": "Occitan", "flag": "🇫🇷"},  # France
+    "oji": {"name": "ᐊᓂᔑᓈᐯᒧᐎᓐ", "flag": "🇨🇦"},  # Canada
+    "ori": {"name": "ଓଡ଼ିଆ", "flag": "🇮🇳"},  # India
+    "orm": {"name": "Afaan Oromoo", "flag": "🇪🇹"},  # Ethiopia
+    "oss": {"name": "Ирон æвзаг", "flag": "🇷🇺"},  # Russia
+    "pan": {"name": "ਪੰਜਾਬੀ", "flag": "🇮🇳"},  # India
+    "per": {"name": "فارسی", "flag": "🇮🇷"},  # Iran
+    "pli": {"name": "पाऴि", "flag": "🏳️"},  # Historical language
+    "pol": {"name": "Polski", "flag": "🇵🇱"},  # Poland
+    "por": {"name": "Português", "flag": "🇵🇹"},  # Portugal
+    "pus": {"name": "پښتو", "flag": "🇦🇫"},  # Afghanistan
+    "que": {"name": "Runa Simi", "flag": "🇵🇪"},  # Peru
+    "roh": {"name": "Rumantsch", "flag": "🇨🇭"},  # Switzerland
+    "ron": {"name": "Română", "flag": "🇷🇴"},  # Romania
+    "rum": {"name": "Română", "flag": "🇷🇴"},  # Also 'ron'
+    "run": {"name": "Kirundi", "flag": "🇧🇮"},  # Burundi
+    "rus": {"name": "Русский", "flag": "🇷🇺"},  # Russia
+    "sag": {"name": "Sängö", "flag": "🇨🇫"},  # Central African Republic
+    "san": {"name": "संस्कृतम्", "flag": "🇮🇳"},  # India
+    "sin": {"name": "සිංහල", "flag": "🇱🇰"},  # Sri Lanka
+    "slk": {"name": "Slovenčina", "flag": "🇸🇰"},  # Slovakia
+    "slo": {"name": "Slovenčina", "flag": "🇸🇰"},  # Also 'slk'
+    "slv": {"name": "Slovenščina", "flag": "🇸🇮"},  # Slovenia
+    "sme": {"name": "Davvisámegiella", "flag": "🇳🇴"},  # Norway
+    "smo": {"name": "Gagana Samoa", "flag": "🇼🇸"},  # Samoa
+    "sna": {"name": "ChiShona", "flag": "🇿🇼"},  # Zimbabwe
+    "snd": {"name": "سنڌي", "flag": "🇵🇰"},  # Pakistan
+    "som": {"name": "Soomaaliga", "flag": "🇸🇴"},  # Somalia
+    "sot": {"name": "Sesotho", "flag": "🇱🇸"},  # Lesotho
+    "spa": {"name": "Español", "flag": "🇪🇸"},  # Spain
+    "sqi": {"name": "Shqip", "flag": "🇦🇱"},  # Albania
+    "srd": {"name": "Sardu", "flag": "🇮🇹"},  # Italy
+    "srp": {"name": "Српски", "flag": "🇷🇸"},  # Serbia
+    "ssw": {"name": "SiSwati", "flag": "🇸🇿"},  # Eswatini
+    "sun": {"name": "Basa Sunda", "flag": "🇮🇩"},  # Indonesia
+    "swa": {"name": "Kiswahili", "flag": "🇰🇪"},  # Kenya
+    "swe": {"name": "Svenska", "flag": "🇸🇪"},  # Sweden
+    "tah": {"name": "Reo Tahiti", "flag": "🇵🇫"},  # French Polynesia
+    "tam": {"name": "தமிழ்", "flag": "🇮🇳"},  # India
+    "tat": {"name": "Татар теле", "flag": "🇷🇺"},  # Russia
+    "tel": {"name": "తెలుగు", "flag": "🇮🇳"},  # India
+    "tgk": {"name": "Тоҷикӣ", "flag": "🇹🇯"},  # Tajikistan
+    "tgl": {"name": "Tagalog", "flag": "🇵🇭"},  # Philippines
+    "tha": {"name": "ไทย", "flag": "🇹🇭"},  # Thailand
+    "tib": {"name": "བོད་ཡིག", "flag": "🇨🇳"},  # China (Tibet)
+    "tir": {"name": "ትግርኛ", "flag": "🇪🇷"},  # Eritrea
+    "ton": {"name": "faka Tonga", "flag": "🇹🇴"},  # Tonga
+    "tsn": {"name": "Setswana", "flag": "🇧🇼"},  # Botswana
+    "tso": {"name": "Xitsonga", "flag": "🇿🇦"},  # South Africa
+    "tuk": {"name": "Türkmençe", "flag": "🇹🇲"},  # Turkmenistan
+    "tur": {"name": "Türkçe", "flag": "🇹🇷"},  # Turkey
+    "twi": {"name": "Twi", "flag": "🇬🇭"},  # Ghana
+    "uig": {"name": "Uyƣurqə", "flag": "🇨🇳"},  # China
+    "ukr": {"name": "Українська", "flag": "🇺🇦"},  # Ukraine
+    "urd": {"name": "اردو", "flag": "🇵🇰"},  # Pakistan
+    "uzb": {"name": "Oʻzbek", "flag": "🇺🇿"},  # Uzbekistan
+    "ven": {"name": "Tshivenḓa", "flag": "🇿🇦"},  # South Africa
+    "vie": {"name": "Tiếng Việt", "flag": "🇻🇳"},  # Vietnam
+    "vol": {"name": "Volapük", "flag": "🏳️"},  # Constructed language
+    "wel": {"name": "Cymraeg", "flag": "🏴󠁧󠁢󠁷󠁬󠁳󠁿"},  # Wales
+    "wln": {"name": "Walon", "flag": "🇧🇪"},  # Belgium
+    "wol": {"name": "Wolof", "flag": "🇸🇳"},  # Senegal
+    "xho": {"name": "isiXhosa", "flag": "🇿🇦"},  # South Africa
+    "yid": {"name": "ייִדיש", "flag": "🏳️"},  # No specific country
+    "yor": {"name": "Yorùbá", "flag": "🇳🇬"},  # Nigeria
+    "zha": {"name": "Saɯ cueŋƅ", "flag": "🇨🇳"},  # China
+    "zho": {"name": "中文", "flag": "🇨🇳"},  # China
+    "zul": {"name": "isiZulu", "flag": "🇿🇦"},  # South Africa
 }
