b622891dbcddca8dcaf26818e63a126cea9731be
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2025-03-04 09:27:39 +0100

    lint: run prettier

--- piggy/static/js/main.js
+++ piggy/static/js/main.js
@@ -9,45 +9,45 @@ const dyslexiaButton = document.getElementById("dyslexia-button");
 // const fontTheme = localStorage.getItem("data-font-theme") || "default";
 
 function pageTransition() {
-    document.body.classList.add("transition");
-    setTimeout(() => {
-        document.body.classList.remove("transition");
-    }, 1000);
+  document.body.classList.add("transition");
+  setTimeout(() => {
+    document.body.classList.remove("transition");
+  }, 1000);
 }
 
 // Function to open the settings menu
 function openSettingsMenu() {
-    settingsMenu.classList.add("open");
+  settingsMenu.classList.add("open");
 }
 
 // Function to close the settings menu
 function closeSettingsMenu() {
-    settingsMenu.classList.remove("open");
+  settingsMenu.classList.remove("open");
 }
 
 function toggleDyslexia() {
-    // Retrieve the current theme from the data attribute
-    let fontTheme = document.documentElement.getAttribute("data-font-theme");
+  // Retrieve the current theme from the data attribute
+  let fontTheme = document.documentElement.getAttribute("data-font-theme");
 
-    // Toggle between 'default' and 'dyslexia'
-    if (fontTheme === "default") {
-        document.documentElement.setAttribute("data-font-theme", "dyslexia");
-        localStorage.setItem("data-font-theme", "dyslexia"); // Save theme to localStorage
-        dyslexiaButton.innerHTML = "Dyslexia Friendly Mode [✅]";
-    } else {
-        document.documentElement.setAttribute("data-font-theme", "default");
-        localStorage.setItem("data-font-theme", "default"); // Save theme to localStorage
-        dyslexiaButton.innerHTML = "Dyslexia Friendly Mode";
-    }
+  // Toggle between 'default' and 'dyslexia'
+  if (fontTheme === "default") {
+    document.documentElement.setAttribute("data-font-theme", "dyslexia");
+    localStorage.setItem("data-font-theme", "dyslexia"); // Save theme to localStorage
+    dyslexiaButton.innerHTML = "Dyslexia Friendly Mode [✅]";
+  } else {
+    document.documentElement.setAttribute("data-font-theme", "default");
+    localStorage.setItem("data-font-theme", "default"); // Save theme to localStorage
+    dyslexiaButton.innerHTML = "Dyslexia Friendly Mode";
+  }
 
-    pageTransition();
+  pageTransition();
 }
 
 // TODO: fix this:
 if (fontTheme === "default") {
-    dyslexiaButton.innerHTML = "Dyslexia Friendly Mode";
+  dyslexiaButton.innerHTML = "Dyslexia Friendly Mode";
 } else {
-    dyslexiaButton.innerHTML = "Dyslexia Friendly Mode [✅]";
+  dyslexiaButton.innerHTML = "Dyslexia Friendly Mode [✅]";
 }
 
 // Set the selected option based on the current theme
@@ -63,22 +63,22 @@ dyslexiaButton.addEventListener("click", toggleDyslexia);
 
 // Event listener for theme selection change
 themeSelect.addEventListener("change", function () {
-    pageTransition();
+  pageTransition();
 
-    const selectedTheme = themeSelect.value;
-    document.documentElement.setAttribute("data-theme", selectedTheme);
-    localStorage.setItem("theme", selectedTheme); // Save theme to localStorage
+  const selectedTheme = themeSelect.value;
+  document.documentElement.setAttribute("data-theme", selectedTheme);
+  localStorage.setItem("theme", selectedTheme); // Save theme to localStorage
 });
 
 // Close settings menu when clicking outside of it
 window.addEventListener("click", function (event) {
-    if (
-        settingsMenu.classList.contains("open") && // Only if the menu is open
-        !settingsMenu.contains(event.target) && // Click is outside the menu
-        event.target !== settingsButton // Click is not on the settings button
-    ) {
-        closeSettingsMenu();
-    }
+  if (
+    settingsMenu.classList.contains("open") && // Only if the menu is open
+    !settingsMenu.contains(event.target) && // Click is outside the menu
+    event.target !== settingsButton // Click is not on the settings button
+  ) {
+    closeSettingsMenu();
+  }
 });
 
 // card search script
