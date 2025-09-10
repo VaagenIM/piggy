19c8d3630d1239d4908a191c0f419f500938e2a3
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2025-09-10 20:44:21 +0200

    feat: use high contrast based on system preference

--- piggy/static/js/on-load.js
+++ piggy/static/js/on-load.js
@@ -1,9 +1,11 @@
-// Initialize theme and font settings asap
 const systemPreferredTheme =
-  window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches
-    ? "dark"
-    : "light";
+  window.matchMedia && window.matchMedia("(prefers-contrast: more)").matches
+    ? "high-contrast"
+    : window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
 
+// Initialize theme and font settings asap
 const currentTheme = localStorage.getItem("theme") || systemPreferredTheme;
 const fontTheme = localStorage.getItem("fontTheme") || "default";
 
