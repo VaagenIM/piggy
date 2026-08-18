4f600cd547eb3bba52daad7e157d8b9dd6b06fed
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2026-08-18 19:48:49 +0200

    chore: testing port to env & remove unused cd command

--- run.py
+++ run.py
@@ -9,6 +9,8 @@ load_dotenv()
 
 subprocesses = []
 
+PORT = os.environ.get("PIGGY_PORT", 5001)
+
 
 @atexit.register
 def cleanup():
@@ -22,7 +24,7 @@ def checkout_branch():
     branch = os.environ.get("PIGGYBANK_BRANCH", "test_output")
     print("Checking out branch: " + branch)
     if "piggybank" in branch:
-        cmd = f"cd piggybank && git fetch && git checkout {branch} && cd .."
+        cmd = f"cd piggybank && git fetch && git checkout {branch}"
     else:
         cmd = f"cd piggybank && git stash && git fetch && git checkout {branch} && git pull"
     subprocess.run(cmd, shell=True, check=True)
@@ -45,7 +47,7 @@ if __name__ == "__main__":
         # This code will run only once, not in the reloaded processes
         checkout_branch()
         subprocesses.append(subprocess.Popen("npx livereload piggy,piggybank -e html,css,js,md", shell=True))
-        print("Houston, we have lift-off! (http://localhost:5001)")
+        print(f"Houston, we have lift-off! (http://localhost:{PORT})")
     # Import after setting the environment variables for testing
     from piggy.app import create_app
     from piggy.devtools import inject_devtools
@@ -53,7 +55,7 @@ if __name__ == "__main__":
     app = create_app(debug=os.environ.get("FLASK_DEBUG", False) == "1")
     inject_devtools(app)
 
-    app.run(port=5001)
+    app.run(port=PORT)
 else:
     # Production
     from piggy.app import create_app
