abb1a3054f4f018c82095a582d7d133b7ecb0caa
Sondre Grønås <44143748+sondregronas@users.noreply.github.com>
2026-09-23 14:33:56 +0200

    fix: fallback to generated thumbnails

--- piggy/piggybank.py
+++ piggy/piggybank.py
@@ -186,11 +186,15 @@ def _register_shortlink(shortlink_map: dict, meta: dict, fallback_path: Path, ta
         else target_path
     )
     description = meta.get("description") or meta.get("oinkdata", {}).get("summary") or meta.get("summary", "")
+    if not meta.get("thumbnail"):
+        thumbnail = f"/api/generate-thumbnail?text={title}"
+    else:
+        thumbnail = f"/{MEDIA_ROUTE}/{media_path}/{meta.get('thumbnail', 'media/header')}.{IMG_FMT}?title={title}"
     shortlink_map[shortlink] = {
         "target": target,
         "title": title,
         "description": description,
-        "image": f"/{MEDIA_ROUTE}/{media_path}/{meta.get('thumbnail', 'media/header')}.{IMG_FMT}?title={title}",
+        "image": thumbnail,
     }
     return shortlink
 
